2f672a49583a34531ec0e641c5a23d9c21db0238
kiryu <kiryu@DESKTOP-SR0JLDE>
2022-05-16 23:41:51 +0500

    Добавьте файлы проекта.

---
+++ Olympiada_1/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Olympiada_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите расположение входных данных:\r\n";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(234, 13);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(405, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Найти ответ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(234, 44);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(406, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(62, 96);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(117, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ответ:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(102, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 26);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Загрузка сортировки\r\nмассива:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(652, 156);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Olympiada_1/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Olympiada_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try//Находим ошибки ввода
+            {             
+                string path = @textBox1.Text;
+                string[] lines = File.ReadAllLines(path);//Читаем все строки в файле и записываем их в массив
+                int kolvo = 0;
+                Int32.TryParse(lines[0], out kolvo);//Находим кол-во строк             
+
+                int[,] rows_and_places = new int[kolvo, 2];//Создаём двухмерный массив рядов и мест
+
+                progressBar1.Maximum = rows_and_places.GetLength(0);//Значение максимума для загрузки
+
+                for (int i = 1; i < lines.Length; i++)//С помощью этого цикла делим числа из строки на ряда и места и записываем в двухмерный массив
+                {
+                    char[] numbers = lines[i].ToCharArray();//Записываем все символы из строки в массив символов
+                    string row = "";
+                    string place = "";
+                    int space = 0;
+                    for (int k = 0; k < numbers.Length; k++)//Ищем пробел в строке и записываем его номер в переменную
+                    {
+                        if (numbers[k] == ' ')
+                        {
+                            space = k;
+                        }
+                    }
+                    for (int l = 0; l < space; l++)// Записываем числа в строку до пробела
+                    {
+                        row += numbers[l];
+                    }
+                    rows_and_places[i - 1, 0] = Int32.Parse(row);//Записываем числа из строки в массив
+
+                    for (int j = space; j < numbers.Length; j++)
+                    {
+                        place += numbers[j];
+                    }
+                    rows_and_places[i - 1, 1] = Int32.Parse(place) * -1;
+
+
+                }
+
+                for (int i = 0; i < rows_and_places.GetLength(0); i++)//Цикл для сортировки массива
+                {
+                    progressBar1.Value += 1;//Добавляется значение после каждого повтора цикла
+                    for (int j = 0; j < rows_and_places.GetLength(0) - 1; j++)
+                    {
+                        //Условия для сортировки столбцов по возрастанию
+                        if (rows_and_places[j, 0] > rows_and_places[j + 1, 0] || rows_and_places[j, 0] == rows_and_places[j + 1, 0] && rows_and_places[j, 1] > rows_and_places[j + 1, 1])
+                        {
+                            for (int c = 0; c < rows_and_places.GetLength(1); c++)//Сортировка методом пузырька
+                            {                               
+                                var temp = rows_and_places[j, c];
+                                rows_and_places[j, c] = rows_and_places[j + 1, c];
+                                rows_and_places[j + 1, c] = temp;
+                            }
+                        }
+                    }
+                }
+
+                int most_row = 0;
+                int least_place = 0;
+                for (int i = 1; i < kolvo - 1; i++)//Находим среди чисел нужные нам значения
+                {
+                    
+                    if (rows_and_places[i, 0] == rows_and_places[i - 1, 0])//Если номер ряда совпадают, то код идёт дальше
+                    {
+                        if ((rows_and_places[i, 1] - rows_and_places[i - 1, 1]) == 14)// Если вычесть из места одного ряда значения другого и получилось 14, тогда записываем ответы в переменную
+                        {
+                            most_row = rows_and_places[i, 0];
+                            least_place = rows_and_places[i, 1] - 1;
+
+                        }
+                    }
+                }
+                textBox2.Text = $"{most_row}, {least_place * (-1)}";//Выводим ответы
+            }
+            catch (IOException)//Если введён неправильный путь, выведет ошибку
+            {
+                MessageBox.Show("Неправильно введён путь файла");
+            }
+            catch (Exception)//Если в файле первая строка неправильного типа, выдаст ошибку
+            {
+                MessageBox.Show("Неправильный тип входных данных");
+            }
+        }
+
+    }
+}
